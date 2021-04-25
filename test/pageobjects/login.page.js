d0b9e2e25fcdbced4518c73678e478bc499f54f9
tatiana <korolevatatiana77@gmail.com>
2021-04-25 10:57:30 -0700

    second commit

--- test/pageobjects/login.page.js
+++ test/pageobjects/login.page.js
@@ -1,31 +1,24 @@
 import Page from './page';
 
-/**
- * sub page containing specific selectors and methods for a specific page
- */
 class LoginPage extends Page {
-    /**
-     * define selectors using getter methods
-     */
-    get inputUsername () { return $('#username') }
-    get inputPassword () { return $('#password') }
-    get btnSubmit () { return $('button[type="submit"]') }
 
-    /**
-     * a method to encapsule automation code to interact with the page
-     * e.g. to login using username and password
-     */
-    async login (username, password) {
-        await (await this.inputUsername).setValue(username);
-        await (await this.inputPassword).setValue(password);
-        await (await this.btnSubmit).click();
+    get inputUsername() { return $('#normal_login_email'); }
+    get inputPassword() { return $('#normal_login_password'); }
+    get btnSubmit() { return $('button[type="submit"]'); }
+
+    async setLogin(email){
+        await this.inputUsername.setValue(email);
+    }
+    async setPassword(password){
+        await this.inputPassword.setValue(password);
     }
 
-    /**
-     * overwrite specifc options to adapt it to page object
-     */
-    open () {
-        return super.open('login');
+    async clickSubmitButton(){
+        await this.btnSubmit.click();
+    }
+
+     open () {
+        return super.open('/user/login');
     }
 }
 
--- test/specs/example.e2e.js
+++ test/specs/example.e2e.js
@@ -1,14 +1,13 @@
 import LoginPage from  '../pageobjects/login.page';
-import SecurePage from '../pageobjects/secure.page';
+import ProfilePage from '../pageobjects/profile.page';
 
-describe('My Login application', () => {
-    it('should login with valid credentials', async () => {
-        await LoginPage.open();
-
-        await LoginPage.login('tomsmith', 'SuperSecretPassword!');
-        await expect(SecurePage.flashAlert).toBeExisting();
-        await expect(SecurePage.flashAlert).toHaveTextContaining(
-            'You logged into a secure area!');
+describe('Auth', () => {
+    it('user logs in with valid data',  () => {
+        LoginPage.open();
+        LoginPage.setLogin('nenobi3527@iludir.com');
+        LoginPage.setPassword('t1875456');
+        LoginPage.clickSubmitButton();
+        ProfilePage.isOpen();
     });
 });
 
